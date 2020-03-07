f64c29cd8f9282ecca794d6267964830445ee6ff
Abdullah <abdullahpm1997@gmail.com>
2020-03-07 12:01:57 -0330

    Adding User Authorization with Google Firebase SDK

---
+++ app/src/main/java/com/example/muneasytravel/UserAuthorization.java
@@ -0,0 +1,81 @@
+package com.example.muneasytravel;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class UserAuthorization extends AppCompatActivity {
+
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_authorization);
+
+        getSupportActionBar().hide();
+
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        mAuth = FirebaseAuth.getInstance();
+
+        if (mAuth.getCurrentUser() != null) {
+            loginUser();
+        }
+
+
+    }
+
+    public void loginUserButton(View view) {
+        mAuth.signInWithEmailAndPassword(emailEditText.getText().toString(), passwordEditText.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success
+                            loginUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(UserAuthorization.this, "Invalid username/password. Please sign up " +
+                                            "if you are a new user.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    public void signupUserButton(View view) {
+        mAuth.createUserWithEmailAndPassword(emailEditText.getText().toString(), passwordEditText.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign up user (Add to Firebase Database)
+                            loginUser();
+                        } else {
+                            Toast.makeText(UserAuthorization.this, "Signup failed. Please try again",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    // move to main activity
+    public void loginUser() {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+    }
+}
